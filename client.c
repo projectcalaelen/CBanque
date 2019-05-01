5bae128079962ca1fa57fd29650f8c6bcda6e20d
TCalaelen <pacome.bouvet@gmail.com>
2019-05-01 13:29:34 +0200

    File for the managing client. More specification to come

---
+++ client.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct client{}
+
+void ajouter_client(){
+
+}
+
+void modifier_client(){
+
+}
+
+void supprimer_client(int id){
+
+}
+
+void rechercher(char* string){
+
+}
