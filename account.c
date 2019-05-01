533b107f0ffaa94ecdffcd26522c4d54c0b1de25
TCalaelen <pacome.bouvet@gmail.com>
2019-05-01 14:12:19 +0200

    File to manage account. More specification to come

---
+++ account.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+strut compte{}
+
+void creer_compte(){
+
+}
+
+void consultation(int id_client){
+
+}
+
+void virement(int id_emetteur, int id_destinataire, int somme){
+
+}
+
+void historique(){
+
+}
