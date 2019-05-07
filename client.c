d742863f8d67c9dec5464bda679998e46c0bbf91
Calaelen <pacome.bouvet@gmail.com>
2019-05-07 14:12:26 +0200

    Ajout de SEARCH_CLIENT Ajout de DELETE_CLIENT Ajout des commentaire

--- client.c
+++ client.c
@@ -56,9 +56,10 @@ Client add_client(int id_client, char const* lastname, char const* firstname, ch
  *		- int int_val		:	Values if it's an int
  *
  *	OUTPUT:
+ *	    - Return the Client* self with modified fields by pointer.
  *		
  */
-void edit_client(Client* self, int field, char* char_val, int int_val){
+void edit_client(Client* self, int field, char const* char_val, int int_val){
 	switch(field){
 		case 1:
 			for(int i=0; i<MAX_CHAR; i++){
@@ -84,15 +85,53 @@ void edit_client(Client* self, int field, char* char_val, int int_val){
 }
 
 
-
-void delete_client(int id){
-
+/*              DELETE_CLIENT
+ *
+ * Delete the client <=> put the id_client at -1
+ *
+ *  INPUT:
+ *      - Client *self      : The Client to be deleted
+ *
+ */
+void delete_client(Client* self){
+    self->id_client = -1;
 }
 
-void search_client(Client** tab, int t_tab, char* string, int t_string){
-	
+/*          SEARCH_CLIENT
+ * Search the result for the string given in the name of list of client.
+ * If the string end with XXX* it will show all the client that start with XXX.
+ *
+ * INPUT:
+ *      - Client** list_client      : list of all the client
+ *      - int t_tab                 : Size of the list of client
+ *      - char *string              : String to look for in the nal
+ *      - int t_string              : Size of the string given
+ *
+ * OUTPUT:
+ *      - Show a list ( lastname + firstname + id_client) of the all the client that matches
+ */
+void search_client(Client** list_client, int t_tab, char* string, int t_string){
+	if("*" == string[t_string]) {
+        string[t_string - 1] = "\0";
+        for (int i = 0; i < t_tab; i++) {
+            if (strncmp(*string, list_client[i]->lastname, t_string - 1) == 0) {
+                printf("%d: %s %s\n", list_client[i]->id_client, list_client[i]->lastname, list_client[i]->firstname);
+            }
+        }
+    }else{
+	    for(int i = 0; i< t_tab; i++){
+	        if(strcmp(*string, list_client[i]->lastname) == 0){
+	            printf("%d: %s %s\n",list_client[i]->id_client, list_client[i]->lastname, list_client[i]->firstname);
+	        }
+	    }
+	}
 }
 
+/*          SHOW_CLIENT
+ *
+ *  Display the variable of the client on standard output
+ *
+ */
 void show_client(Client* self){
     printf("Client N°%d\n", self->id_client);
     printf("Nom : %s\n", self->lastname);
@@ -106,11 +145,14 @@ int main(){
     char prenom[MAX_CHAR] = "Pacome";
     char profession[MAX_CHAR] = "Etudiant";
     int tel = 683602417;
-    Client client = add_client(1, nom, prenom, profession, tel);
-    show_client(&client);
-    char nom_bis[MAX_CHAR] = "Byszewski";
-    edit_client(&client, 2, nom_bis, 0);
-    printf("\n");
-    show_client(&client);
+    Client list_client[15];
+    list_client[0] = add_client(1, nom, prenom, profession, tel);
+    *nom = "Bysewski";
+    *prenom = "Cedric";
+    list_client[1] = add_client(2, nom, prenom, profession, tel);
+    *nom = "Delagarde";
+    *prenom = "Aurelien";
+    list_client[2] = add_client(3, nom, prenom, profession, tel);
+
 
 }
