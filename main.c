999c2e9646f84669bf55aec98c38eec2553aa4ea
Pacome Bouvet <pacome.bouvet@gmail.com>
2019-07-15 23:53:13 +0200

    Debuggage main.c

--- main.c
+++ main.c
@@ -70,18 +70,22 @@ void menu_client(Client *client_list, int *nb_client, int *nb_max_client){
     printf("1. Ajouter un nouveau client\n");
     printf("2. Supprimer un client\n");
     printf("3. Consulter un client\n");
+    printf("4. Editer un client\n");
     printf("0. Revenir au menu principal");
     scanf("%d", &choix);
     switch(choix){
       case 1:
-        add_client(client_list, &nb_client);
+        add_client(client_list, nb_client);
         nb_client++;
         break;
       case 2:
-        del_client(client_list, &nb_client);
+        del_client(client_list, nb_client);
         break;
       case 3:
         search_client(client_list, nb_client);
+        break;
+      case 4:
+        edit_client(client_list, nb_client);
         break;
       case 0:
         return;
@@ -105,7 +109,7 @@ void menu_compte(Account *account_list, int* nb_compte, int* nb_max_compte){
     printf("Veuillez choisir une option:\n");
     printf("1. Creer un compte\n");
     printf("2. Supprimer un compte\n");
-    printf("3. Gerer un compte\n");
+    printf("3. Afficher compte d'un client\n");
     printf("0. Revenir au menu précedent\n");
     scanf("%d", &choix);
     switch(choix){
@@ -117,7 +121,7 @@ void menu_compte(Account *account_list, int* nb_compte, int* nb_max_compte){
         del_account(account_list, &nb_compte);
         break;
       case 3:
-        manage_account(account_list, nb_compte);
+        show_account(account_list, nb_compte);
         break;
       case 0:
         return;
@@ -135,6 +139,30 @@ void menu_compte(Account *account_list, int* nb_compte, int* nb_max_compte){
 
 
 int main(){
+  char chaine[MAX_CHAR];
+  t_account type_list[4];
+  int control = 0, i = 0;
+  FILE* file = fopen("config.ini","r");
+  if(file == NULL){
+    printf("Erreur Lecture fichier");
+    return EXIT_FAILURE;
+  }
+  while(fgets(chaine, MAX_CHAR, file) != NULL){
+    switch(control%3){
+      case 0:
+        strcpy(type_list[i].name, chaine);
+        break;
+      case 1:
+        type_list[i].taux = atoi(chaine);
+        break;
+      case 2:
+        type_list[i].time = atoi(chaine);
+        i++;
+        break;
+    }
+    control++;
+  }
+  fclose(file);
   Client *client_list = NULL;
   client_list = malloc(100 * sizeof(Client));
   Account *account_list = NULL;
@@ -178,6 +206,9 @@ int main(){
         break;
     }
   }
+  free(client_list);
+  free(account_list);
+  free(type_list);
   printf("Merci d'avoir utilisez CBanque");
   return EXIT_SUCCESS;
 }
